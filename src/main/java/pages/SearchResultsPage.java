ff8811b69962325078576ed930262349f53b194d
AsheeshD <ad.victor@gmail.com>
2025-02-10 12:05:17 +0530

    Update SearchResultsPage.java
    Used PageFactory.initElements(driver, this); for element initialization.

--- src/main/java/pages/SearchResultsPage.java
+++ src/main/java/pages/SearchResultsPage.java
@@ -3,6 +3,8 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
@@ -10,21 +12,27 @@ public class SearchResultsPage {
     private WebDriver driver;
     private WebDriverWait wait;
 
-    private By firstResult = By.xpath("//h3[contains(text(),'Employee Education in 2018: Strategies to Watch')]");
-    private By searchButton = By.xpath("//button[@type='submit']");
+    // Using @FindBy for static elements
+    @FindBy(xpath = "//button[@type='submit']")
+    private WebElement searchButton;
 
+    // Keeping By locator for dynamic elements
+    private By firstResult = By.xpath("//h3[contains(text(),'Employee Education in 2018: Strategies to Watch')]");
+
+    // Constructor with PageFactory initialization
     public SearchResultsPage(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
         this.wait = wait;
+        PageFactory.initElements(driver, this);
     }
 
+    // Click on the search button
     public void clickSearchButton() {
-        WebElement searchButtonElement = wait.until(ExpectedConditions.visibilityOfElementLocated(searchButton));
-        searchButtonElement.click();
+        wait.until(ExpectedConditions.elementToBeClickable(searchButton)).click();
     }
 
+    // Check if the first result is displayed
     public boolean isFirstResultDisplayed() {
-        WebElement firstResultElement = wait.until(ExpectedConditions.visibilityOfElementLocated(firstResult));
-        return firstResultElement.isDisplayed();
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(firstResult)).isDisplayed();
     }
 }
