79c490b4ca75ee7e2df0405c37a4061ec082ec66
AsheeshD <ad.victor@gmail.com>
2025-02-10 11:55:07 +0530

    Update HomePage.java
    code optimized using PageFactory.initElements(driver, this); for element initialization.

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -4,23 +4,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
     private WebDriver driver;
     private WebDriverWait wait;
 
+    // Using @FindBy for static elements
+    @FindBy(className = "nav-link-search")
+    private WebElement searchIcon;
+
+    // Using By locators for dynamic elements
     private By acceptCookiesButton = By.id("onetrust-accept-btn-handler");
-    private By searchIcon = By.className("nav-link-search");
     private By searchField = By.id("search-field");
 
+    // Constructor with PageFactory initialization
     public HomePage(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
         this.wait = wait;
-
+        PageFactory.initElements(driver, this);
     }
 
+    // Handling dynamic element using explicit wait
     public void acceptCookies() {
         try {
             WebElement acceptCookiesButtonElement = wait.until(ExpectedConditions.elementToBeClickable(acceptCookiesButton));
@@ -30,12 +38,13 @@ public class HomePage {
         }
     }
 
+    // Clicking search icon using JavaScript Executor (static element)
     public void clickSearchIcon() {
-        WebElement searchIconElement = driver.findElement(searchIcon);
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].click();", searchIconElement);
+        js.executeScript("arguments[0].click();", searchIcon);
     }
 
+    // Handling search field (dynamic element) with explicit wait
     public void enterSearchQuery(String query) {
         WebElement searchFieldElement = wait.until(ExpectedConditions.visibilityOfElementLocated(searchField));
         searchFieldElement.sendKeys(query);
